a72adb02aca9bef77286c8b720a65641f9d3b186
selgas <s.elgas@techsmith.com>
2024-03-07 16:47:38 -0500

    Fixed an issue with tile hit detection

--- FinalQuestino/physics.c
+++ FinalQuestino/physics.c
@@ -9,43 +9,41 @@ void cPhysics_Init( cPhysics_t* physics )
 
 void cPhysics_MovePlayer( cGame_t* game )
 {
-  cVector2f_t prevPlayerPos;
+  cVector2f_t newPos;
   cPlayer_t* player = &( game->player );
   uint8_t tileRowStartIndex, tileRowEndIndex, tileColStartIndex, tileColEndIndex, row, col, tile;
 
-  prevPlayerPos.x = player->position.x;
-  prevPlayerPos.y = player->position.y;
-  player->position.x += ( player->velocity.x * game->clock.frameSeconds );
-  player->position.y += ( player->velocity.y * game->clock.frameSeconds );
+  newPos.x = player->position.x + ( player->velocity.x * game->clock.frameSeconds );
+  newPos.y = player->position.y + ( player->velocity.y * game->clock.frameSeconds );
 
   // clip to screen boundaries
-  if ( player->position.x < 0 )
+  if ( newPos.x < 0 )
   {
-    player->position.x = 0;
+    newPos.x = COLLISION_PADDING;
   }
-  else if ( ( player->position.x + player->hitBoxSize.x ) >= ( TILES_X * TILE_SIZE ) )
+  else if ( ( newPos.x + player->hitBoxSize.x ) >= ( TILES_X * TILE_SIZE ) )
   {
-    player->position.x = ( TILES_X * TILE_SIZE ) - player->hitBoxSize.x - COLLISION_PADDING;
+    newPos.x = ( TILES_X * TILE_SIZE ) - player->hitBoxSize.x - COLLISION_PADDING;
   }
-  if ( player->position.y < 0 )
+  if ( newPos.y < 0 )
   {
-    player->position.y = 0;
+    newPos.y = COLLISION_PADDING;
   }
-  else if ( ( player->position.y + player->hitBoxSize.y ) >= ( TILES_Y * TILE_SIZE ) )
+  else if ( ( newPos.y + player->hitBoxSize.y ) >= ( TILES_Y * TILE_SIZE ) )
   {
-    player->position.y = ( TILES_Y * TILE_SIZE ) - player->hitBoxSize.y - COLLISION_PADDING;
+    newPos.y = ( TILES_Y * TILE_SIZE ) - player->hitBoxSize.y - COLLISION_PADDING;
   }
 
   // clip to unpassable horizontal tiles
-  if ( player->position.x != prevPlayerPos.x )
+  if ( newPos.x != player->position.x )
   {
     tileRowStartIndex = player->position.y / TILE_SIZE;
     tileRowEndIndex = ( player->position.y + player->hitBoxSize.y ) / TILE_SIZE;
 
-    if ( player->position.x < prevPlayerPos.x )
+    if ( newPos.x < player->position.x )
     {
       // moving left, check leftward tiles
-      col = player->position.x / TILE_SIZE;
+      col = newPos.x / TILE_SIZE;
 
       for( row = tileRowStartIndex; row <= tileRowEndIndex; row++ )
       {
@@ -53,7 +51,7 @@ void cPhysics_MovePlayer( cGame_t* game )
 
         if ( !( tile & TILE_PASSABLE_FLAG ) )
         {
-          player->position.x = ( ( col + 1 ) * TILE_SIZE );
+          newPos.x = ( ( col + 1 ) * TILE_SIZE );
           break;
         }
       }
@@ -61,15 +59,15 @@ void cPhysics_MovePlayer( cGame_t* game )
     else
     {
       // moving right, check rightward tiles
-      col = ( player->position.x + player->hitBoxSize.x ) / TILE_SIZE;
+      col = ( newPos.x + player->hitBoxSize.x ) / TILE_SIZE;
 
       for( row = tileRowStartIndex; row <= tileRowEndIndex; row++ )
       {
-        tile = game->tileMap.tiles[ col + ( row * TILES_X ) ];
+        tile = game->tileMap.tiles[col + ( row * TILES_X )];
 
         if ( !( tile & TILE_PASSABLE_FLAG ) )
         {
-          player->position.x = ( col * TILE_SIZE ) - player->hitBoxSize.x - COLLISION_PADDING;
+          newPos.x = ( col * TILE_SIZE ) - player->hitBoxSize.x - COLLISION_PADDING;
           break;
         }
       }
@@ -77,15 +75,15 @@ void cPhysics_MovePlayer( cGame_t* game )
   }
 
   // clip to unpassable vertical tiles
-  if ( player->position.y != prevPlayerPos.y )
+  if ( newPos.y != player->position.y )
   {
     tileColStartIndex = player->position.x / TILE_SIZE;
     tileColEndIndex = ( player->position.x + player->hitBoxSize.x ) / TILE_SIZE;
 
-    if ( player->position.y < prevPlayerPos.y )
+    if ( newPos.y < player->position.y )
     {
       // moving up, check upward tiles
-      row = player->position.y / TILE_SIZE;
+      row = newPos.y / TILE_SIZE;
 
       for( col = tileColStartIndex; col <= tileColEndIndex; col++ )
       {
@@ -93,7 +91,7 @@ void cPhysics_MovePlayer( cGame_t* game )
 
         if ( !( tile & TILE_PASSABLE_FLAG ) )
         {
-          player->position.y = ( ( row + 1 ) * TILE_SIZE );
+          newPos.y = ( ( row + 1 ) * TILE_SIZE );
           break;
         }
       }
@@ -101,34 +99,32 @@ void cPhysics_MovePlayer( cGame_t* game )
     else
     {
       // moving down, check downward tiles
-      row = ( player->position.y + player->hitBoxSize.y ) / TILE_SIZE;
+      row = ( newPos.y + player->hitBoxSize.y ) / TILE_SIZE;
 
       for( col = tileColStartIndex; col <= tileColEndIndex; col++ )
       {
-        tile = game->tileMap.tiles[ col + ( row * TILES_X ) ];
+        tile = game->tileMap.tiles[col + ( row * TILES_X )];
 
         if ( !( tile & TILE_PASSABLE_FLAG ) )
         {
-          player->position.y = ( row * TILE_SIZE ) - player->hitBoxSize.y - COLLISION_PADDING;
+          newPos.y = ( row * TILE_SIZE ) - player->hitBoxSize.y - COLLISION_PADDING;
           break;
         }
       }
     }
   }
 
-  player->velocity.x = 0;
-  player->velocity.y = 0;
-
-  if ( prevPlayerPos.x != player->position.x || prevPlayerPos.y != player->position.y ||
+  if ( newPos.x != player->position.x || newPos.y != player->position.y ||
        player->sprite.currentFrame != game->physics.spriteFrameCache )
   {
-    cScreen_WipeSprite( &( game->screen ), &( game->tileMap ),
-                        prevPlayerPos.x + player->spriteOffset.x,
-                        prevPlayerPos.y + player->spriteOffset.y );
+    cScreen_WipeSprite( &( game->screen ), &( game->tileMap ), player->position.x + player->spriteOffset.x, player->position.y + player->spriteOffset.y );
     game->physics.spriteFrameCache = player->sprite.currentFrame;
   }
 
-  cScreen_DrawSprite( &( game->screen ), &( player->sprite ), &( game->tileMap ),
-                      player->position.x + player->spriteOffset.x,
-                      player->position.y + player->spriteOffset.y );
+  player->position.x = newPos.x;
+  player->position.y = newPos.y;
+  cScreen_DrawSprite( &( game->screen ), &( player->sprite ), &( game->tileMap ), player->position.x + player->spriteOffset.x, player->position.y + player->spriteOffset.y );
+
+  player->velocity.x = 0;
+  player->velocity.y = 0;
 }
