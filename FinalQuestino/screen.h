471e495ba748f040d952b21240a8c7349f7f82ce
selgas <s.elgas@techsmith.com>
2024-05-10 11:02:45 -0400

    Make menus and text box dark gray
    This will make them easier to see in caves

--- FinalQuestino/screen.h
+++ FinalQuestino/screen.h
@@ -8,14 +8,15 @@
 #include "common.h"
 
 // possibly useful color macros
-#define	BLACK   0x0000
-#define	BLUE    0x001F
-#define	RED     0xF800
-#define	GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+#define BLACK     0x0000
+#define DARKGRAY  0x4228
+#define BLUE      0x001F
+#define RED       0xF800
+#define GREEN     0x07E0
+#define CYAN      0x07FF
+#define MAGENTA   0xF81F
+#define YELLOW    0xFFE0
+#define WHITE     0xFFFF
 
 #define TRANSPARENT_COLOR MAGENTA
 
